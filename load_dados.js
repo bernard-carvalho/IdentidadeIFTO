d2674504176fa8501dd4f98e04b71a15742bd8d6
bernard.carvalho <bernard.carvalho@ifto.edu.br>
2022-11-16 10:22:43 -0300

    first commit

---
+++ load_dados.js
@@ -0,0 +1,139 @@
+var dados = [
+    {
+        "bloco":"16",
+        "eventos":[
+            {
+                "local":"1601",
+                "nome":"Apresentações orais - TRABALHO DE EXTENSÃO",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1602",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS EXATAS E DA TERRA",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1603",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS BIOLÓGICAS",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1604",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - ENGENHARIAS",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1605",
+                "nome":"OFICINA - Empreendedorismo Juvenil: para além da sala de aula",
+                "horario":"08:00 as 12:00"
+            },
+        ],
+        "x":"-10.198646",
+        "y":"-48.311418"
+    },
+    {
+        "bloco":"17",
+        "eventos":[
+            {
+                "local":"1701",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS DA SAUDE",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1702",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS AGRÁRIAS",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1703",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS SOCIAIS APLICADAS",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1705",
+                "nome":"OFICINA - Estratégia de Marketing : o uso das ferramentas digitais a seu favor",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1706",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - CIÊNCIAS HUMANAS",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"1707",
+                "nome":"Apresentações orais - TRABALHO CIENTÍFICO - LINGUISTICA, LETRAS E ARTES",
+                "horario":"08:00 as 12:00"
+            },
+            {
+                "local":"-",
+                "nome":"Roda de conversa - PESQUISARAU - Histórias do Tocantins (Professor Gleuter)",
+                "horario":"17:00 as 17:40"
+            },
+            {
+                "local":"-",
+                "nome":"Vernissage (Mostra de Artes Visuais) - Exposição permanente durante os 3 dias do evento",
+                "horario":"17:40 as 18:00"
+            },
+        ],
+        "x":"-10.198841",
+        "y":"-48.311412"
+    },
+    {
+        "bloco":"Auditório central",
+        "eventos":[
+            {
+                "local":"-",
+                "nome":"Abertura do IFestival - Grupo Convidado Balé Popular do Tocantins",
+                "horario":"14:00 as 14:15"
+            },
+            {
+                "local":"-",
+                "nome":"Mostra de Dança",
+                "horario":"14:30 as 16:00"
+            },
+            {
+                "local":"-",
+                "nome":"Mostra de Teatro",
+                "horario":"16:00 as 17:00"
+            },
+            {
+                "local":"-",
+                "nome":"Cerimônia de abertura",
+                "horario":"19:00 as 22:00"
+            },
+        ],
+        "x":"-10.199522",
+        "y":"-48.312415"
+    },
+    {
+        "bloco":"Ginásio poliesportivo",
+        "eventos":[
+            {
+                "local":"-",
+                "nome":"Escritório de Projetos",
+                "horario":"08:00 as 22:00"
+            },
+            {
+                "local":"-",
+                "nome":"Ideathon",
+                "horario":"08:00 as 22:00"
+            },
+        ],
+        "x":"-10.199564",
+        "y":"-48.310854"
+    },
+    {
+        "bloco":"9",
+        "eventos":[
+            {
+                "local":"Mini auditório 9",
+                "nome":"Fórum de Pesquisadores",
+                "horario":"14:00 as 17:00"
+            },
+        ],
+        "x":"-10.200351",
+        "y":"-48.311504"
+    },
+    
+];
+localStorage.setItem("dados",JSON.stringify(dados));
